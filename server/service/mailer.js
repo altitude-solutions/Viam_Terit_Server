13bcdb713a9a61a65cd6a1ae78297f2d0910505f
Javier Contreras <javier.contreras@altitudesolutions.org>
2020-03-02 09:56:02 -0400

    mail everymonday at 8 AM

--- server/service/mailer.js
+++ server/service/mailer.js
@@ -25,9 +25,19 @@ const schedule = require('node-schedule');
 
 let timeZoneOffset = process.env.TIME_ZONE_OFFSET || 0;
 
-// run every monday 
-let j = schedule.scheduleJob('* * * * * 1', function () {
-// let j = schedule.scheduleJob('0 * * * * *', function () {
+
+// *    *    *    *    *    *
+// ┬    ┬    ┬    ┬    ┬    ┬
+// │    │    │    │    │    │
+// │    │    │    │    │    └ day of week (0 - 7) (0 or 7 is Sun)
+// │    │    │    │    └───── month (1 - 12)
+// │    │    │    └────────── day of month (1 - 31)
+// │    │    └─────────────── hour (0 - 23)
+// │    └──────────────────── minute (0 - 59)
+// └───────────────────────── second (0 - 59, OPTIONAL)
+
+// run every monday at 12(-4) = 8
+let j = schedule.scheduleJob('0 0 12 * * 1', function () {
     let today = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), -timeZoneOffset, 0, 0, 0).getTime();
     let oneWeeksMore = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate() + 7, -timeZoneOffset, 0, 0, 0).getTime();
     let twoWeeksMore = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate() + 14, -timeZoneOffset, 0, 0, 0).getTime();
